e089ccae5e592c2d8dcdc989d9e86692a5ed1512
hareets <mharits14@gmail.com>
2023-07-05 22:06:07 +0700

    add pages: speakers
    Tambah halaman speakers

---
+++ src/components/SpeakerContent.vue
@@ -0,0 +1,132 @@
+<!-- <template>
+  <section
+    id="slider"
+    class="coming-soon slider-element min-vh-100 bgchanger include-header bg-dark hero"
+  >
+    
+  </section>
+</template> -->
+
+<template>
+  <!-- Coming soon
+    ============================================= -->
+  <section
+    id="slider"
+    class="coming-soon slider-element min-vh-100 bgchanger include-header bg-dark hero"
+  >
+    <div class="slider-inner">
+      <div class="vertical-middle slider-element-fade">
+        <div class="content-wrap pb-0">
+          <div class="container mb-5">
+            <div class="heading-block">
+              <h2
+                class="nott ls0 fw-semibold mb-3 animate__animated animate__fadeInUp animate__delay-100ms"
+              >
+                Conference Speaker
+              </h2>
+            </div>
+            <div class="row col-mb-50 mb-0">
+              <div
+                class="col-lg-3 col-md-6 animate__animated animate__fadeInUp animate__delay-200ms"
+              >
+                <div class="team">
+                  <div class="team-image">
+                    <img
+                      src="../frontend/assets/images/eriktohir.jpg"
+                      alt="Erick Tohir"
+                      style="height: 356px; object-fit: cover"
+                    />
+                  </div>
+                  <div class="team-desc text-start">
+                    <div class="team-title">
+                      <h3 class="nott ls0 fw-semibold">Erick Tohir</h3>
+                      <h4 class="fw-medium color mb-0">
+                        Minister of State Owned Enterprises Indonesia's
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                class="col-lg-3 col-md-6 animate__animated animate__fadeInUp animate__delay-300ms"
+              >
+                <div class="team">
+                  <div class="team-image">
+                    <img
+                      src="../frontend/assets/images/pahala.jpg"
+                      alt="Pahala Mansury"
+                      style="height: 356px; object-fit: cover"
+                    />
+                  </div>
+                  <div class="team-desc text-start">
+                    <div class="team-title">
+                      <h3 class="nott ls0 fw-semibold">Pahala Mansury</h3>
+                      <h4 class="fw-medium color mb-0">
+                        Vice Minister of State Owned Entripreses Indonesia's
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                class="col-lg-3 col-md-6 animate__animated animate__fadeInUp animate__delay-400ms"
+              >
+                <div class="team">
+                  <div class="team-image">
+                    <img
+                      src="../frontend/assets/images/tedy.png"
+                      alt="Tedy Bharata"
+                      style="height: 356px; object-fit: cover"
+                    />
+                  </div>
+                  <div class="team-desc text-start">
+                    <div class="team-title">
+                      <h3 class="nott ls0 fw-semibold">Tedy Bharata</h3>
+                      <h4 class="fw-medium color mb-0">
+                        Deputy of Human Resources and IT
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                class="col-lg-3 col-md-6 animate__animated animate__fadeInUp animate__delay-500ms"
+              >
+                <div class="team">
+                  <div class="team-image">
+                    <img
+                      src="../frontend/assets/images/ayubi.jpg"
+                      alt="Soleh Ayubi"
+                      style="height: 356px; object-fit: cover"
+                    />
+                  </div>
+                  <div class="team-desc text-start">
+                    <div class="team-title">
+                      <h3 class="nott ls0 fw-semibold">Soleh Ayubi</h3>
+                      <h4 class="fw-medium color mb-0">
+                        Deputy Chief Executive Officer PT Biofarma
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="center mt-6 mb-6 wow fadeInUp" data-wow-delay="100ms">
+              <h4>Stay tuned, more speakers will announced soon</h4>
+              <!-- <h3 class="text-capitalize fw-bold font-body">
+                                    Can’t find the answer?
+                                </h3>
+                                <RouterLink to="/contact">
+                                    <button class="button button-large button-circle button-large fw-medium">Contact Us</button>
+                                </RouterLink> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- #Coming soon end -->
+</template>
